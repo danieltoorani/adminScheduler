5e4cf4a56726b9a07d8243ea168c80c3e37408c6
Daniel Toorani <dtoorani@ucdavis.edu>
2018-01-15 20:44:36 -0800

    Update models.js

--- clean_server/resources/app/models/models.js
+++ clean_server/resources/app/models/models.js
@@ -1,6 +1,7 @@
 var UserMeta = require('./User.js'),
     connection = require('../sequelize.js')
 //here we define our connection to user table using sequelize
+//and make use of attributes defined in User.js
 var User = connection.define('users', UserMeta.attributes, UserMeta.options)
 
 // you can define relationships here
